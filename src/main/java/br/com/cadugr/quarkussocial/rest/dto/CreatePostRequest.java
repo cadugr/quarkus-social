da11673a66c9dedacff336a3b59b4a43ef619798
Cadu Guerra <cadu.guerra@gmail.com>
2023-06-26 21:11:14 -0300

    Criando cenário com beforeEach

--- src/main/java/br/com/cadugr/quarkussocial/rest/dto/CreatePostRequest.java
+++ src/main/java/br/com/cadugr/quarkussocial/rest/dto/CreatePostRequest.java
@@ -1,8 +1,15 @@
 package br.com.cadugr.quarkussocial.rest.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreatePostRequest {
   private String text;
 }
---
+++ src/test/java/br/com/cadugr/quarkussocial/rest/PostResourceTest.java
@@ -0,0 +1,50 @@
+package br.com.cadugr.quarkussocial.rest;
+
+import static io.restassured.RestAssured.given;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import br.com.cadugr.quarkussocial.domain.model.User;
+import br.com.cadugr.quarkussocial.domain.repository.UserRepository;
+import br.com.cadugr.quarkussocial.rest.dto.CreatePostRequest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
+@QuarkusTest
+@TestHTTPEndpoint(PostResource.class)
+public class PostResourceTest {
+
+  @Inject
+  UserRepository userRepository;
+  Long userId;
+
+  @BeforeEach
+  @Transactional
+  public void setUP() {
+    var user = User.builder().age(30).name("Fulano").build();
+    userRepository.persist(user);
+    userId = user.getId();
+  }
+
+  @Test
+  @DisplayName("Should create a post for a user")
+  public void createPostTest() {
+   var postRequest = CreatePostRequest.builder().text("Some text").build();
+
+    given()
+      .contentType(ContentType.JSON)
+      .body(postRequest)
+      .pathParam("userId", userId)
+    .when()
+      .post()
+    .then()
+      .statusCode(201);    
+  }
+  
+}
